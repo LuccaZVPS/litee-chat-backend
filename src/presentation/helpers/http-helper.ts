8fb77edf57c2e9293086fcffa75441d3fd10362b
Lucca <luccazvps@gmail.com>
2023-02-17 15:44:39 -0300

    feat: add http helper metohod

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,37 @@
+import { ServerError } from "../errors/server-error";
+
+export const ok = (data: any) => {
+  return {
+    body: data,
+    statusCode: 200,
+  };
+};
+export const badRequest = (data: any) => {
+  return {
+    body: data,
+    statusCode: 400,
+  };
+};
+
+export const created = (data: any) => {
+  return {
+    body: data,
+    statusCode: 204,
+  };
+};
+export const unauthorized = (data: any) => {
+  return {
+    body: data,
+    statusCode: 401,
+  };
+};
+export const forbidden = (data: any) => {
+  return {
+    body: data,
+    statusCode: 403,
+  };
+};
+export const serverError = () => ({
+  statusCode: 500,
+  body: new ServerError(),
+});
