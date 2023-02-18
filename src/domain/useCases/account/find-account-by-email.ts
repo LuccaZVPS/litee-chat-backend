6d140e21d1fa4a66cdd1e039e36c09d67a3f1519
Lucca <luccazvps@gmail.com>
2023-02-18 19:38:03 -0300

    fix: change wrong import

--- src/domain/useCases/account/find-account-by-email.ts
+++ src/domain/useCases/account/find-account-by-email.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from "@/domain/models/account";
+import { AccountModel } from "../../models/account";
 
 export interface FindAccountByEmail {
   findByEmail(email: string): Promise<AccountModel>;
