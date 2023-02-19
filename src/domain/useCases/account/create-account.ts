7dee6ca01ccb3f1efffe7a47137658d6bd12f071
Lucca <luccazvps@gmail.com>
2023-02-19 10:28:50 -0300

    refactor: return account id in createAccount use case

--- src/domain/useCases/account/create-account.ts
+++ src/domain/useCases/account/create-account.ts
@@ -1,5 +1,5 @@
 import { CreateAccountDTO } from "../../../presentation/controllers/account/DTOs/create-account-dto";
 
 export interface CreateAccount {
-  create(account: CreateAccountDTO): Promise<void>;
+  create(account: CreateAccountDTO): Promise<{ _id: string }>;
 }
