66e15e4954f3c07bcee5b3eb6f931684851b07fc
Lucca <luccazvps@gmail.com>
2023-02-20 19:00:43 -0300

    feat: ensure EmailVerifyController calls validate verify method with correct values

--- src/presentation/controllers/account/email-verify-controller.ts
+++ src/presentation/controllers/account/email-verify-controller.ts
@@ -1,3 +1,4 @@
+import { EmailVerify } from "../../../domain/useCases/account/email-verify";
 import { InvalidBody } from "../../errors/invalid-body-error";
 import { badRequest, serverError } from "../../helpers/http-helper";
 import {
@@ -8,7 +9,10 @@ import {
 import { Validator } from "../../protocols/validator";
 import { VerifyEmailDTO } from "./DTOs/verify-email-dto";
 export class EmailVerifyController implements Controller {
-  constructor(private readonly validator: Validator) {}
+  constructor(
+    private readonly validator: Validator,
+    private readonly emailVerify: EmailVerify
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const verifyEmailDTO = new VerifyEmailDTO();
@@ -21,6 +25,10 @@ export class EmailVerifyController implements Controller {
       if (errors) {
         return badRequest(new InvalidBody(errors));
       }
+      await this.emailVerify.verify(
+        verifyEmailDTO._id,
+        verifyEmailDTO.password
+      );
       return { statusCode: 0, body: "" };
     } catch {
       return serverError();
