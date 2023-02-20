20ab51dbf39f61c12d21f7ac54e93033ccedb9ce
Lucca <luccazvps@gmail.com>
2023-02-20 18:50:44 -0300

    test: ensure EmailVerifyController return server error if validade throws

--- tests/presentation/account/verify-email-controller.spec.ts
+++ tests/presentation/account/verify-email-controller.spec.ts
@@ -1,6 +1,9 @@
 import { EmailVerifyController } from "../../../src/presentation/controllers/account/email-verify-controller";
 import { InvalidBody } from "../../../src/presentation/errors/invalid-body-error";
-import { badRequest } from "../../../src/presentation/helpers/http-helper";
+import {
+  badRequest,
+  serverError,
+} from "../../../src/presentation/helpers/http-helper";
 import { Validator } from "../../../src/presentation/protocols/validator";
 
 describe("Verify email controller", () => {
@@ -36,5 +39,13 @@ describe("Verify email controller", () => {
     });
     const response = await sut.handle({ body: { ...verifyEmailDTO } });
     expect(response).toEqual(badRequest(new InvalidBody("any_error")));
+  });
+  test("should return serverError if validate throws", async () => {
+    const { sut, validatorStub } = makeSut();
+    jest.spyOn(validatorStub, "validate").mockImplementationOnce(async () => {
+      throw new Error();
+    });
+    const response = await sut.handle({ body: { ...verifyEmailDTO } });
+    expect(response).toEqual(serverError());
   });
 });
