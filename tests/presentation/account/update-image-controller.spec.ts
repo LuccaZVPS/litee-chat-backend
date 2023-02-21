80c2e35e76a6b38ee427c4a459ceeec424471bf8
Lucca <luccazvps@gmail.com>
2023-02-21 20:52:11 -0300

    test: ensure updateImageController call fileType method with correct value

---
+++ tests/presentation/account/update-image-controller.spec.ts
@@ -0,0 +1,26 @@
+import { UpdateImageController } from "../../../src/presentation/controllers/account/update-image-controller";
+import { FileType } from "../../../src/presentation/protocols/file-type";
+
+describe("Update image controller", () => {
+  const makeFileTypeStub = () => {
+    class FileTypeStub implements FileType {
+      async type(): Promise<string> {
+        return "img";
+      }
+    }
+    return new FileTypeStub();
+  };
+  const makeSut = () => {
+    const fileTypeStub = makeFileTypeStub();
+    return {
+      fileTypeStub,
+      sut: new UpdateImageController(fileTypeStub),
+    };
+  };
+  test("should call file type method with correct value", async () => {
+    const { sut, fileTypeStub } = makeSut();
+    const spy = jest.spyOn(fileTypeStub, "type");
+    await sut.handle({ file: { path: "any_file_path" } });
+    expect(spy).toBeCalledWith("any_file_path");
+  });
+});
