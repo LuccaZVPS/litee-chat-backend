0deca60be2c31989d5ad41d2b18c2b5c921a5914
Lucca <luccazvps@gmail.com>
2023-02-21 21:19:36 -0300

    feat: ensure updateImageController call unlink with correct path if file extension is not allowed

--- src/presentation/controllers/account/update-image-controller.ts
+++ src/presentation/controllers/account/update-image-controller.ts
@@ -2,7 +2,7 @@ import { InvalidBody } from "../../errors/invalid-body-error";
 import { badRequest, serverError } from "../../helpers/http-helper";
 import { Controller, HttpResponse } from "../../protocols/controller";
 import { FileType } from "../../protocols/file-type";
-
+import { unlinkSync } from "fs";
 export class UpdateImageController implements Controller {
   constructor(private readonly fileType: FileType) {}
   async handle(httpRequest: any): Promise<HttpResponse> {
@@ -10,6 +10,7 @@ export class UpdateImageController implements Controller {
       const type = await this.fileType.type(httpRequest?.file?.path);
       const whiteList = ["img", "png", "jpg", "jpeg"];
       if (!whiteList.includes(type)) {
+        unlinkFile.unlink(httpRequest.file.path);
         return badRequest(new InvalidBody("file extension not allowed"));
       }
       return;
@@ -18,3 +19,8 @@ export class UpdateImageController implements Controller {
     }
   }
 }
+export const unlinkFile = {
+  unlink(path: string) {
+    unlinkSync(path);
+  },
+};
