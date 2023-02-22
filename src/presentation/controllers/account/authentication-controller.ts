685e09d7bc404d22076d731bfee9f04d77e990ea
Lucca <luccazvps@gmail.com>
2023-02-22 20:17:34 -0300

    feat: ensure AuthenticationController return badRequest if validate return errors

--- src/presentation/controllers/account/authentication-controller.ts
+++ src/presentation/controllers/account/authentication-controller.ts
@@ -1,6 +1,11 @@
 import { FindAccountByEmail } from "../../../domain/useCases/account/find-account-by-email";
 import { UnauthorizedError } from "../../errors/unauthorized-error";
-import { ok, serverError, unauthorized } from "../../helpers/http-helper";
+import {
+  badRequest,
+  ok,
+  serverError,
+  unauthorized,
+} from "../../helpers/http-helper";
 import {
   Controller,
   HttpRequest,
@@ -9,11 +14,11 @@ import {
 import { Validator } from "../../protocols/validator";
 import { AuthenticationDTO } from "./DTOs/authentication-dto";
 import { Authentication } from "../../../domain/useCases/account/authentication";
+import { InvalidBody } from "../../errors/invalid-body-error";
 
 export class AuthenticationController implements Controller {
   constructor(
     private readonly validator: Validator,
-    private readonly findAccountByEmail: FindAccountByEmail,
     private readonly authentication: Authentication
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -24,12 +29,9 @@ export class AuthenticationController implements Controller {
           authenticationDTO[field] = httpRequest.body[field];
         }
       }
-      await this.validator.validate(authenticationDTO);
-      const accountFound = await this.findAccountByEmail.findByEmail(
-        authenticationDTO.email
-      );
-      if (!accountFound) {
-        return unauthorized(new UnauthorizedError());
+      const { errors } = await this.validator.validate(authenticationDTO);
+      if (errors) {
+        return badRequest(new InvalidBody(errors));
       }
       const isCorrect = await this.authentication.auth(
         authenticationDTO.email,
