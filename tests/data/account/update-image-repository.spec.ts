f8e0e94e0b2b5a082ddad5124507535c7fb05054
Lucca <luccazvps@gmail.com>
2023-02-22 18:40:58 -0300

    test: ensure UpdateImage call update method with correct value

---
+++ tests/data/account/update-image-repository.spec.ts
@@ -0,0 +1,26 @@
+import { UpdateImageRepository } from "../../../src/data/protocols/account/update-image-repository";
+import { UpdateImage } from "../../../src/data/useCases/account/update-image";
+
+describe("UpdateImage", () => {
+  const makeUpdateImageRepositoryStub = () => {
+    class UpdateImageRepositoryStub implements UpdateImageRepository {
+      async update(): Promise<void> {
+        return;
+      }
+    }
+    return new UpdateImageRepositoryStub();
+  };
+  const makeSut = () => {
+    const updateImageRepositoryStub = makeUpdateImageRepositoryStub();
+    return {
+      updateImageRepositoryStub,
+      sut: new UpdateImage(updateImageRepositoryStub),
+    };
+  };
+  test("should call updateImage with correct value", async () => {
+    const { sut, updateImageRepositoryStub } = makeSut();
+    const spy = jest.spyOn(updateImageRepositoryStub, "update");
+    await sut.update("any_id", "any_path");
+    expect(spy).toHaveBeenCalledWith("any_id", "any_path");
+  });
+});
