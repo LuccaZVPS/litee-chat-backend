40d51c56492b95374aeb5c963ed03ae26eb6bd85
Lucca <luccazvps@gmail.com>
2023-02-22 18:40:51 -0300

    feat: ensure UpdateImage call update method with correct value

---
+++ src/data/useCases/account/update-image.ts
@@ -0,0 +1,10 @@
+import { UpdateImage as UpdateImageType } from "../../../domain/useCases/account/update-image";
+import { UpdateImageRepository } from "../../protocols/account/update-image-repository";
+
+export class UpdateImage implements UpdateImageType {
+  constructor(private readonly updateImageRepository: UpdateImageRepository) {}
+  async update(_id: string, path: string): Promise<void> {
+    await this.updateImageRepository.update(_id, path);
+    return;
+  }
+}
