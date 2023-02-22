460d9f483a84c89ff1b11b3cf76f5383cd46f6b0
Lucca <luccazvps@gmail.com>
2023-02-22 15:40:02 -0300

    test: ensure FindAccountByEmail call find method with correct value

---
+++ tests/data/account/find-account-by-email.spec.ts
@@ -0,0 +1,32 @@
+import { FindAccountByEmailRepository } from "../../../src/data/protocols/account/find-account-by-email-repository";
+import { FindAccountByEmail } from "../../../src/data/useCases/account/find-account-by-email";
+import { AccountModel } from "../../../src/domain/models/account";
+import { anyAccount } from "../../presentation/account/mocks/fake-account";
+import { faker } from "@faker-js/faker";
+describe("FindAccountByEmail", () => {
+  const makeFindAccountByEmailRepository = () => {
+    class FindAccountByEmailRepositoryStub
+      implements FindAccountByEmailRepository
+    {
+      async find(): Promise<void | AccountModel> {
+        return anyAccount;
+      }
+    }
+    return new FindAccountByEmailRepositoryStub();
+  };
+  const makeSut = () => {
+    const findAccountByEmailRepository = makeFindAccountByEmailRepository();
+    return {
+      findAccountByEmailRepository,
+      sut: new FindAccountByEmail(findAccountByEmailRepository),
+    };
+  };
+  const emailMock = faker.internet.email();
+
+  test("should call find method with correct value", async () => {
+    const { sut, findAccountByEmailRepository } = makeSut();
+    const spy = jest.spyOn(findAccountByEmailRepository, "find");
+    await sut.findByEmail(emailMock);
+    expect(spy).toHaveBeenCalledWith(emailMock);
+  });
+});
