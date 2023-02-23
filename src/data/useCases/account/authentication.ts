0e2fdc38b55991edbad2a60d06d44fe697efd96b
Lucca <luccazvps@gmail.com>
2023-02-23 19:02:30 -0300

    feat: ensure Authentication call find method with correct value

---
+++ src/data/useCases/account/authentication.ts
@@ -0,0 +1,15 @@
+import { Authentication as AuthenticationType } from "../../../domain/useCases/account/authentication";
+import { AccountSession } from "../../../domain/useCases/account/create-account";
+import { CompareHash } from "../../protocols/account/compare-hash";
+import { FindAccountByEmailRepository } from "../../protocols/account/find-account-by-email-repository";
+
+export class Authentication implements AuthenticationType {
+  constructor(
+    private readonly findAccountByEmailRepository: FindAccountByEmailRepository,
+    private readonly compareHash: CompareHash
+  ) {}
+  async auth(email: string, password: string): Promise<false | AccountSession> {
+    await this.findAccountByEmailRepository.find(email);
+    return;
+  }
+}
