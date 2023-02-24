ccd5695714ff665288a3091f4bcb6428dbbdae07
Lucca <luccazvps@gmail.com>
2023-02-24 11:19:59 -0300

    feat: ensure CreateAccount calls generatePassword method

--- src/data/useCases/account/create-account.ts
+++ src/data/useCases/account/create-account.ts
@@ -5,11 +5,17 @@ import {
 } from "../../../domain/useCases/account/create-account";
 import { CreateAccountRepository } from "../../protocols/account/create-account-repository";
 import { Hasher } from "../../protocols/account/hasher";
+import { GeneratePassword } from "../../protocols/account/generate-password";
+import { CreateVerificationRepository } from "../../protocols/account/create-verification-repository";
+import { SendVerificationEmail } from "../../protocols/account/send-verification-emai";
 
 export class CreateAccount implements CreateAccountType {
   constructor(
     private readonly hasher: Hasher,
-    private readonly createAccountRepository: CreateAccountRepository
+    private readonly createAccountRepository: CreateAccountRepository,
+    private readonly generatePassword: GeneratePassword,
+    private readonly createVerificationRepository: CreateVerificationRepository,
+    private readonly sendVerificationEmail: SendVerificationEmail
   ) {}
   async create(account: CreateAccountDTO): Promise<AccountSession> {
     const hashedPassword = this.hasher.hash(account.password);
@@ -20,6 +26,7 @@ export class CreateAccount implements CreateAccountType {
     if (!accountData._id) {
       throw new Error();
     }
+    const generatedPassword = this.generatePassword.generate();
     return accountData;
   }
 }
