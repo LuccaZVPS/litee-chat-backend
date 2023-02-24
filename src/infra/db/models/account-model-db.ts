eaf57f99e145476abe3d877a4cf941de80f177bf
Lucca <luccazvps@gmail.com>
2023-02-24 09:57:31 -0300

    fix: add verified field

--- src/infra/db/models/account-model-db.ts
+++ src/infra/db/models/account-model-db.ts
@@ -7,6 +7,7 @@ export const accountSchema = new Mongoose.Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   imageURL: { type: String },
+  verified: { type: Boolean, default: false },
   friends: [
     {
       type: Mongoose.Schema.Types.String,
