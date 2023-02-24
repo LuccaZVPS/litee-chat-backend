11d819673cf73bfbd839f37cec843159b9920920
Lucca <luccazvps@gmail.com>
2023-02-24 14:20:00 -0300

    feat: ensure FindVerificationRepository call findOne with correct value

--- src/infra/db/repositories/email-verify-repository.ts
+++ src/infra/db/repositories/email-verify-repository.ts
@@ -1,7 +1,9 @@
 import { FindVerificationRepository } from "../../../data/protocols/account/find-verification-repository";
+import { emailVerifyModel } from "../models/email-verify-model-db";
 
 export class EmailVerifyRepository implements FindVerificationRepository {
-  find(_id: string): Promise<string | void> {
+  async find(_id: string): Promise<string | void> {
+    await emailVerifyModel.findOne({ accountId: _id });
     return;
   }
 }
