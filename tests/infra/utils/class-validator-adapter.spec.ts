f99a2d2fc36c1298cd27f5bd5a461ead691ff37d
Lucca <luccazvps@gmail.com>
2023-02-24 18:52:57 -0300

    test: ensure ClassValidatorAdapter return the same value as validate method

--- tests/infra/utils/class-validator-adapter.spec.ts
+++ tests/infra/utils/class-validator-adapter.spec.ts
@@ -1,3 +1,4 @@
+import { ValidationError } from "class-validator";
 import {
   ClassValidatorAdapter,
   method,
@@ -18,11 +19,20 @@ describe("ClassValidatorAdapter", () => {
   });
   test("should throws if validate method throws", async () => {
     const { sut } = makeSut();
-    jest.spyOn(method, "validate").mockImplementationOnce(() => {
+    jest.spyOn(method, "validate").mockImplementationOnce(async () => {
       throw new Error();
     });
     const dto = new CreateAccountDTO();
     const response = sut.validate(dto);
     expect(response).rejects.toThrow(new Error());
+  });
+  test("should return the same values as validate", async () => {
+    const { sut } = makeSut();
+    jest.spyOn(method, "validate").mockImplementationOnce(async () => {
+      return "any" as unknown as ValidationError[];
+    });
+    const dto = new CreateAccountDTO();
+    const response = await sut.validate(dto);
+    expect(response).toBe("any");
   });
 });
