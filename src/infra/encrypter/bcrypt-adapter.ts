ef599773b907f484798ccda5cbc908b98910af11
Lucca <luccazvps@gmail.com>
2023-02-24 13:11:44 -0300

    feat: ensure CompareHash cals compareSync with correct value

--- src/infra/encrypter/bcrypt-adapter.ts
+++ src/infra/encrypter/bcrypt-adapter.ts
@@ -1,7 +1,12 @@
 import { Hasher } from "../../data/protocols/account/hasher";
 import bcryptjs from "bcryptjs";
-export class BcryptAdapter implements Hasher {
+import { CompareHash } from "../../data/protocols/account/compare-hash";
+export class BcryptAdapter implements Hasher, CompareHash {
   hash(str: string): string {
     return bcryptjs.hashSync(str);
+  }
+  compare(str: string, hash: string): boolean {
+    bcryptjs.compareSync(str, hash);
+    return;
   }
 }
