8ceddee8c215fec0b1fbf347df783bdc9cac27b3
Lucca <luccazvps@gmail.com>
2023-02-24 14:41:34 -0300

    feat: ensure CreateVerificationRepository call create method with correct value

--- src/infra/db/repositories/email-verify-repository.ts
+++ src/infra/db/repositories/email-verify-repository.ts
@@ -1,7 +1,10 @@
+import { CreateVerificationRepository } from "../../../data/protocols/account/create-verification-repository";
 import { FindVerificationRepository } from "../../../data/protocols/account/find-verification-repository";
 import { emailVerifyModel } from "../models/email-verify-model-db";
 
-export class EmailVerifyRepository implements FindVerificationRepository {
+export class EmailVerifyRepository
+  implements FindVerificationRepository, CreateVerificationRepository
+{
   async find(_id: string): Promise<string | void> {
     const verificationFound = await emailVerifyModel.findOne({
       accountId: _id,
@@ -10,5 +13,9 @@ export class EmailVerifyRepository implements FindVerificationRepository {
       return;
     }
     return verificationFound.secret;
+  }
+  async create(accountId: string, secret: string): Promise<void> {
+    await emailVerifyModel.create({ accountId, secret });
+    return;
   }
 }
