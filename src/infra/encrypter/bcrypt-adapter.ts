6ef1f234ff1b7b81f3b8cd3ab61ab3c147da50c0
Lucca <luccazvps@gmail.com>
2023-02-24 12:30:05 -0300

    feat: ensure Hasher return the same value as hashSync

--- src/infra/encrypter/bcrypt-adapter.ts
+++ src/infra/encrypter/bcrypt-adapter.ts
@@ -2,7 +2,6 @@ import { Hasher } from "../../data/protocols/account/hasher";
 import bcryptjs from "bcryptjs";
 export class BcryptAdapter implements Hasher {
   hash(str: string): string {
-    bcryptjs.hashSync(str);
-    return;
+    return bcryptjs.hashSync(str);
   }
 }
