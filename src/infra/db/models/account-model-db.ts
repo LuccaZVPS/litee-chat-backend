c4efc4891ae7961abcd8160c8e81cabe5e176e08
Lucca <luccazvps@gmail.com>
2023-02-24 08:44:09 -0300

    feat: add mongodb account model

---
+++ src/infra/db/models/account-model-db.ts
@@ -0,0 +1,23 @@
+import { randomUUID } from "crypto";
+import Mongoose from "mongoose";
+
+export const accountSchema = new Mongoose.Schema({
+  _id: { type: String, unique: true, default: randomUUID },
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  imageURL: { type: String },
+  friends: [
+    {
+      type: Mongoose.Schema.Types.String,
+      ref: "friends",
+    },
+  ],
+  requests: [
+    {
+      type: Mongoose.Schema.Types.String,
+      ref: "requests",
+    },
+  ],
+});
+export const accountModel = Mongoose.model("accounts", accountSchema);
