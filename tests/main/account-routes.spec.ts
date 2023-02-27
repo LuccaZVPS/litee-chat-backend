f0e2d6c5452cab1b8fe718827dfda25855cba2a6
Lucca <luccazvps@gmail.com>
2023-02-27 07:39:09 -0300

    test: ensure signup route return 400 if invalid dto is provided

---
+++ tests/main/account-routes.spec.ts
@@ -0,0 +1,38 @@
+import { mongoHelper } from "../../src/infra/db/connection";
+import { accountModel } from "../../src/infra/db/models/account-model-db";
+import request from "supertest";
+import app from "../../src/main/config/app";
+import { createDTO } from "../presentation/account/mocks/create-dto";
+describe("Account routes", () => {
+  beforeAll(async () => {
+    await mongoHelper.connect(process.env.MONGO_URL);
+  });
+  afterAll(async () => {
+    await mongoHelper.close();
+  });
+  afterEach(async () => {
+    await accountModel.deleteMany();
+  });
+
+  describe("signup", () => {
+    const validCreateDTO = { ...createDTO, password: "Valid123456" };
+    test("should return 400 if invalid body is provided", async () => {
+      await request(app)
+        .post("/api/account/signup")
+        .send({ ...validCreateDTO, password: "invalid" })
+        .expect(400);
+      await request(app)
+        .post("/api/account/signup")
+        .send({ ...validCreateDTO, email: "invalid.com" })
+        .expect(400);
+      await request(app)
+        .post("/api/account/signup")
+        .send({ ...validCreateDTO, name: "ab" })
+        .expect(400);
+      await request(app)
+        .post("/api/account/signup")
+        .send({ ...validCreateDTO, name: "name_bigger_than_12_characters" })
+        .expect(400);
+    });
+  });
+});
