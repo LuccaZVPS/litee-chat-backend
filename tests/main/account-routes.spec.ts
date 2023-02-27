f0376e71905a2377eba997d0a52d4d527e6dc198
Lucca <luccazvps@gmail.com>
2023-02-27 08:03:42 -0300

    test: add tests for login route

--- tests/main/account-routes.spec.ts
+++ tests/main/account-routes.spec.ts
@@ -3,6 +3,7 @@ import { accountModel } from "../../src/infra/db/models/account-model-db";
 import request from "supertest";
 import app from "../../src/main/config/app";
 import { createDTO } from "../presentation/account/mocks/create-dto";
+import { faker } from "@faker-js/faker";
 describe("Account routes", () => {
   beforeAll(async () => {
     await mongoHelper.connect(process.env.MONGO_URL);
@@ -49,6 +50,41 @@ describe("Account routes", () => {
         .post("/api/account/signup")
         .send({ ...validCreateDTO, password: "validPassword12" })
         .expect(204);
+    });
+  });
+  describe("login", () => {
+    test("should return 400 if invalid body is provided", async () => {
+      await request(app)
+        .post("/api/account/login")
+        .send({ email: "invalid_email.com", password: "validPassword123" })
+        .expect(400);
+      await request(app)
+        .post("/api/account/login")
+        .send({ email: faker.internet.email(), password: "invalid" })
+        .expect(400);
+    });
+    test("should return 401 if the account provided not exist", async () => {
+      await request(app)
+        .post("/api/account/login")
+        .send({ email: faker.internet.email(), password: "validPassword123" })
+        .expect(401);
+    });
+    test("should return 200 if the account provided exist", async () => {
+      await request(app)
+        .post("/api/account/signup")
+        .send({
+          name: "lucca",
+          email: "any@gmail.com",
+          password: "validPassword123",
+        })
+        .expect(204);
+      await request(app)
+        .post("/api/account/login")
+        .send({
+          email: "any@gmail.com",
+          password: "validPassword123",
+        })
+        .expect(200);
     });
   });
 });
