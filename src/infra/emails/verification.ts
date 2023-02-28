350ffddae498f26f4a38eae8ab0f70d624c628de
Lucca <luccazvps@gmail.com>
2023-02-28 16:25:21 -0300

    feat: add email verification html template

--- src/infra/emails/verification.ts
+++ src/infra/emails/verification.ts
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer";
 import { SendVerificationEmail } from "../../data/protocols/account/send-verification-emai";
+import { makeVerificationTemplate } from "./templates/verification-template";
 // async..await is not allowed in global scope, must use a wrapper
 export class SendEmail implements SendVerificationEmail {
   async send(
@@ -9,22 +10,25 @@ export class SendEmail implements SendVerificationEmail {
     password: string
   ): Promise<void> {
     try {
-      const testAccount = await nodemailer.createTestAccount();
       const transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
+        host: process.env.EMAIL_HOST,
+        port: Number(process.env.EMAIL_PORT),
         secure: false,
         auth: {
-          user: testAccount.user,
-          pass: testAccount.pass,
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
         },
       });
       await transporter.sendMail({
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        from: `Litee Chat" <${process.env.EMAIL_USERNAME}>`, // sender address
         to: email, // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: ``, // html body
+        subject: "Email verification", // Subject line
+        html: makeVerificationTemplate(
+          _id,
+          password,
+          name,
+          process.env.FRONT_END_DOMAIN
+        ), // html body
       });
     } catch (e) {
       console.log(e);
