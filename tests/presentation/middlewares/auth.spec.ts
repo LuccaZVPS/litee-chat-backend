6051411d7820d6c455244cb27c3ac15b4ab34f4f
Lucca <luccazvps@gmail.com>
2023-03-01 12:00:02 -0300

    feat: ensure auth middleware returns 200 if an account is saved in session

--- tests/presentation/middlewares/auth.spec.ts
+++ tests/presentation/middlewares/auth.spec.ts
@@ -1,5 +1,8 @@
 import { UnauthorizedError } from "../../../src/presentation/errors/unauthorized-error";
-import { unauthorized } from "../../../src/presentation/helpers/http-helper";
+import {
+  ok,
+  unauthorized,
+} from "../../../src/presentation/helpers/http-helper";
 import { AuthMiddleware } from "../../../src/presentation/middlewares/auth-middleware";
 
 describe("Auth middleware", () => {
@@ -16,5 +19,16 @@ describe("Auth middleware", () => {
       },
     });
     expect(response).toEqual(unauthorized(new UnauthorizedError()));
+  });
+  test("should return 200 if account is saved in session", async () => {
+    const { sut } = makeSut();
+    const response = await sut.run({
+      body: {
+        session: {
+          account: { _id: "any_id" },
+        },
+      },
+    });
+    expect(response).toEqual(ok(""));
   });
 });
