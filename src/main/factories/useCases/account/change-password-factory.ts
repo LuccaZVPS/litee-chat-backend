88c73086965d72a44cf3272a3a5132baed84e665
Lucca <luccazvps@gmail.com>
2023-03-07 12:19:15 -0300

    feat: update changePassword factory to use new dependency

--- src/main/factories/useCases/account/change-password-factory.ts
+++ src/main/factories/useCases/account/change-password-factory.ts
@@ -1,8 +1,13 @@
 import { ChangePassword } from "../../../../data/useCases/account/change-password";
 import { BcryptAdapter } from "../../../../infra/encrypter/bcrypt-adapter";
 import { makeAccountRepository } from "../../repositories/account-repository";
+import { makeChangePasswordRequestRepository } from "../../repositories/change-password-request-repository-factory";
 export const makeChangePassword = () => {
   const hasher = new BcryptAdapter();
 
-  return new ChangePassword(hasher, makeAccountRepository());
+  return new ChangePassword(
+    hasher,
+    makeAccountRepository(),
+    makeChangePasswordRequestRepository()
+  );
 };
