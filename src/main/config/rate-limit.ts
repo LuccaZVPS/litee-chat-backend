24de5c6123fdadc4914f756cbb513757fa0f6db1
Lucca <luccazvps@gmail.com>
2023-03-08 12:17:36 -0300

    feat: use mongo to save rate-limit cache

--- src/main/config/rate-limit.ts
+++ src/main/config/rate-limit.ts
@@ -1,9 +1,16 @@
 import rateLimit from "express-rate-limit";
+import MongoStore from "rate-limit-mongo";
 export const makeRateLimit = (max: number, minutes: number) => {
   return rateLimit({
     windowMs: minutes * 60 * 1000,
     max,
     standardHeaders: true,
     legacyHeaders: false,
+    store: new MongoStore({
+      uri: process.env.MONGODB_URL,
+      expireTimeMs: minutes * 60 * 1000,
+      errorHandler: console.error.bind(null, "rate-limit-mongo"),
+      // see Configuration section for more options and details
+    }),
   });
 };
--- src/main/index.ts
+++ src/main/index.ts
@@ -2,12 +2,10 @@ import { mongoHelper } from "../infra/db/connection";
 import app from "./config/app";
 import { config } from "dotenv";
 config();
-
-mongoHelper
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    app.listen(process.env.SERVER_PORT, () => {
-      console.log(`server running on ${process.env.SERVER_PORT}`);
-    });
-  })
-  .catch((e) => console.log(e));
+const startup = async () => {
+  await mongoHelper.connect(process.env.MONGODB_URL);
+  app.listen(process.env.SERVER_PORT, () => {
+    console.log(`server running on ${process.env.SERVER_PORT}`);
+  });
+};
+startup();
